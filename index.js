aefc50cdedf3d33bdf21ba5005313c5a999f3d3a
Madeliene <madelienestiller@gmail.com>
2023-11-29 09:35:17 +1000

    context lab done

--- index.js
+++ index.js
@@ -1,5 +1,82 @@
 /* Your Code Here */
 
+const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+};
+
+
+const createEmployeeRecords = (employeeDetails) =>
+    employeeDetails.map(createEmployeeRecord);
+
+
+const createTimeInEvent = (employeeRecord = {}, dateTime) => {
+    const [date, hour] = dateTime.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: Number(hour),
+        date,
+    });
+
+    return this;
+};
+
+const createTimeOutEvent = (employeeRecord = {}, dateTime) => {
+    const [date, hour] = dateTime.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: Number(hour),
+        date,
+    });
+
+    return this;
+};
+
+const hoursWorkedOnDate = function (date) {
+    const timeInEvent = this.timeInEvents.find(
+        (event) => event.date === date
+    );
+
+    const timeOutEvent = this.timeOutEvents.find(
+        (event) => event.date === date
+    );
+
+    if (!timeInEvent) {
+        console.error(`No time-in event found for date: ${date}`);
+        return;
+    }
+
+    if (!timeOutEvent) {
+        console.error(`No time-out event found for date: ${date}`);
+        return;
+    }
+
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+
+    return hoursWorked;
+};
+
+const wagesEarnedOnDate = function (date) {
+    const hoursWorked = this.hoursWorkedOnDate(date);
+    const wage = this.payPerHour;
+
+    return hoursWorked * wage;
+};
+
+//   const allWagesFor = (employeeRecord) => {
+//     return employeeRecord.timeInEvents.reduce((totalWage, timeInEvent) => {
+//       return totalWage + wagesEarnedOnDate(employeeRecord, timeInEvent.date);
+//     }, 0);
+//   };
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +97,10 @@ const allWagesFor = function () {
 
     return payable
 }
+
+const calculatePayroll = (employeeRecords) => {
+    return employeeRecords.reduce((totalWages, employee) => {
+        return totalWages + allWagesFor(employee);
+    }, 0);
+};
 
